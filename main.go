b504975e13b16501bb84f25f5543f244cf7805c1
educative <educative@educative.io>
2022-04-15 17:10:42 +0300

    Add viper to load configuration

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/snirkop89/simplebank/api"
+	db "github.com/snirkop89/simplebank/db/sqlc"
+	"github.com/snirkop89/simplebank/util"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to the database")
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ setup_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SB_PGUSER="root"
+SB_PGPASS="secret"
+SB_PGPORT="5455"
+
+export SB_PGUSER
+export SB_PGPASS
+export SB_PGPORT
