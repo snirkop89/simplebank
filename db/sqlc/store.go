c5c9a73dc9e0f222bfa1229d146fedf2e7cc85de
educative <educative@educative.io>
2022-04-18 08:44:49 +0300

    emit interface and mock db

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,21 +6,27 @@ import (
 	"fmt"
 )
 
+// Store provides all functions to execute db queries and transactions
+type Store interface {
+	Querier
+	TransferTX(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
 // Store provides all functions to execute db quries and transactions
-type Store struct {
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // execTx executes a function within a database transaction
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
@@ -57,7 +63,7 @@ var txKey = struct{}{}
 
 // TransferTx performs a money tranfer from one account to the other.
 // It create a transfer record, add ccount entries, and update accountts' balance within a single database transaction
-func (store *Store) TransferTX(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTX(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTx(ctx, func(q *Queries) error {
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.7.7
+	github.com/golang/mock v1.4.4
 	github.com/lib/pq v1.10.5
+	github.com/spf13/viper v1.11.0
 	github.com/stretchr/testify v1.7.1
 )
 
@@ -31,7 +33,6 @@ require (
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.11.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
