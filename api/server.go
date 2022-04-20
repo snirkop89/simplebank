006ef3a49b51b6040dbf2ef8756931a54338afe2
educative <educative@educative.io>
2022-04-20 09:13:56 +0300

    Add user creation api

--- api/server.go
+++ api/server.go
@@ -22,6 +22,8 @@ func NewServer(store db.Store) *Server {
 		v.RegisterValidation("currency", validCurrencty)
 	}
 
+	router.POST("/users", server.createUser)
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
