443640237e0abce0309c00642a8611a3dc2296cf
Snir Koppelman <snirkwork@gmail.com>
2023-02-17 15:48:46 +0200

    Add test for createuser

---
+++ api/user_test.go
@@ -0,0 +1,56 @@
+package api
+
+import (
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	mockdb "github.com/snirkop89/simplebank/db/mock"
+	db "github.com/snirkop89/simplebank/db/sqlc"
+	"github.com/snirkop89/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateUserAPI(t *testing.T) {
+	user, password := randomUser(t)
+
+	testCases := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: gin.H{
+				"username":  user.Username,
+				"password":  password,
+				"full_name": user.FullName,
+				"email":     user.Email,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {})
+	}
+}
+
+func randomUser(t *testing.T) (user db.User, password string) {
+	hashedPassword, err := util.HashPassword(password)
+	require.NoError(t, err)
+	require.NotEmpty(t, hashedPassword)
+
+	user = db.User{
+		Username:          util.RandomOwner(),
+		HashedPassword:    hashedPassword,
+		FullName:          util.RandomOwner(),
+		Email:             util.RandomEmail(),
+		PasswordChangedAt: time.Now(),
+		CreatedAt:         time.Now(),
+	}
+
+	return user, util.RandomString(10)
+
+}
