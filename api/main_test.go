da42c55225ad9734ff5106e0a6e6a7973b9fb578
Snir Koppelman <snirkwork@gmail.com>
2023-02-19 23:26:29 +0200

    Add login endpoint

--- api/main_test.go
+++ api/main_test.go
@@ -3,10 +3,25 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	db "github.com/snirkop89/simplebank/db/sqlc"
+	"github.com/snirkop89/simplebank/util"
+	"github.com/stretchr/testify/assert"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	assert.NoError(t, err)
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
