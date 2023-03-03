990cdc9e2d38adbe38f07d000cf5e9171b252cf9
Snir Koppelman <snirkwork@gmail.com>
2023-03-03 12:47:03 +0200

    Add CreateUser and LoginUser grpc handlers

---
+++ gapi/converter.go
@@ -0,0 +1,16 @@
+package gapi
+
+import (
+	db "github.com/snirkop89/simplebank/db/sqlc"
+	"github.com/snirkop89/simplebank/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+	}
+}
